562c3e85d950291ed811c81bb5a91e274198c5ac
Facundo Diaz Martinez <fdiaz@fu.do>
2022-12-04 17:24:58 -0300

    fix: add types to previous commit

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,4 @@
 import RedisClient from "./redis/redis.client";
+import { EventClient as eventClient } from "./event.client";
+export type EventClient = eventClient;
 export declare const RedisEventClient: typeof RedisClient;
