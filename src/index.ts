202e59aaa8619694ce324176c24bed4713fb45b0
Facundo Diaz Martinez <fdiaz@fu.do>
2022-12-04 17:20:58 -0300

    feat: add EventClient type

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import RedisClient from "./redis/redis.client";
+import { EventClient as eventClient } from "./event.client";
+
+export type EventClient = eventClient;
 
 export const RedisEventClient = RedisClient;
