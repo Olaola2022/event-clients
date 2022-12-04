4eebd1ad3a5467b4ff742dc143f1d1f900b7c9d4
Facundo Diaz Martinez <fdiaz@fu.do>
2022-12-04 15:47:04 -0300

    feat: redis client as message queue

---
+++ lib/event.client.d.ts
@@ -0,0 +1,10 @@
+export type EventPayload = {
+    uuid: string;
+    body: string;
+};
+export interface EventClient {
+    start(topics: Record<string, string[]>): Promise<void>;
+    subscribe(main: string, topic: string, callback: (payload: unknown) => Promise<void>): Promise<void>;
+    emit(main: string, topic: string, payload: unknown): void;
+    topics(): string[];
+}
---
+++ lib/index.d.ts
@@ -0,0 +1,2 @@
+import RedisClient from "./redis/redis.client";
+export declare const RedisEventClient: typeof RedisClient;
